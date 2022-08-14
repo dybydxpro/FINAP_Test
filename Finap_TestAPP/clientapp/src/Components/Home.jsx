1e4f11ec7a250ea4c29c861217a1cb5a47ada64b
Tharindu Theekshana Dayananda <65506774+venom0039@users.noreply.github.com>
2022-08-14 09:22:52 +0530

    Commit 14-08-2022 09:25AM

--- Finap_TestAPP/clientapp/src/Components/Home.jsx
+++ Finap_TestAPP/clientapp/src/Components/Home.jsx
@@ -1,3 +1,4 @@
+import { Container } from "react-bootstrap";
 import SlideBar from "./SideBar";
 
 export default function Home(){
@@ -7,7 +8,16 @@ export default function Home(){
                 <div className="col-2">
                     <SlideBar />
                 </div>
-                <div className="col"></div>
+                <div className="col">
+                    <Container>
+                        <div className="shadow p-3 mb-5 bg-light rounded my-5 my-4 mx-5">
+                            <div className="">
+                                <h1 className="text-center">Finap Test</h1>
+                            </div>
+                            
+                        </div>
+                    </Container>
+                </div>
             </div>
         </div>
     );
